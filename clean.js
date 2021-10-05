7d1d8704ee14ec9a010fb0d0a5898255fada21a2
Thomas <thomas.lefloch@eisti.fr>
2021-10-05 12:24:04 +0200

    adding multi-user parsing - ty Alecs for the tips

--- clean.js
+++ clean.js
@@ -1,4 +1,4 @@
-const settingslist = require('./settings.json');
+const settings = require('./settings.json');
 const moment = require('moment');
 const fetch = require('node-fetch');
 const ics = require('ics');
@@ -34,12 +34,9 @@ function get_rf(t) {
     return t.split('<input name="__RequestVerificationToken" type="hidden" value="')[1].split('" /></form>')[0];
 }
 
-async function get_fid(t) {
-    return (await t.text()).split("var federationIdStr = '")[1].split("'")[0];
-}
 
 async function get_antiforgerystuff() {
-    let r = await do_request(settingslist.login_url);
+    let r = await do_request(settings.login_url);
     if (r.ok) {
         return [r.headers.get("set-cookie").split(";")[0], get_rf(await r.text())];
     }
@@ -48,47 +45,48 @@ async function get_antiforgerystuff() {
 function get_lc(r) {
     return r.headers.get("set-cookie").split(";")[0];
 }
-settingslist.dataArray.forEach(settings => {
 (async() => {
     console.log("Reaching website.");
     let af = await get_antiforgerystuff();
     console.log("Logging in...");
-    let res = await do_request(settingslist.auth_url,
+    let res = await do_request(settings.auth_url,
         "POST", {
             cookie: af[0],
             "content-type": "application/x-www-form-urlencoded"
         },
         `Name=${encodeURIComponent(settings.login)}&Password=${encodeURIComponent(settings.password)}&__RequestVerificationToken=${encodeURIComponent(af[1])}`
     );
-    console.log("Extracting federation id");
-    let fid = await get_fid(
-        await do_request(settingslist.calendar_url, "GET", { cookie: `${af[0]};${get_lc(res)}` }, `vt=month`)
-    );
-    console.log("Extracting calendar data...");
-    res = await do_request(settingslist.response_url, "POST", { cookie: `${af[0]};${get_lc(res)}` }, `start=${moment().format("YYYY-MM-DD")}&end=${moment().add(1, "year").format("YYYY")}-12-31&resType=104&calView=month&federationIds%5B%5D=${fid}`);
-    console.log("Parsing data...");
-    let data = (await res.json()).map(x => {
-        return {
-            "start": moment(x.start).format('YYYY-M-D-H-m').split("-"),
-            "end": moment(x.end).format('YYYY-M-D-H-m').split("-").length > 2 ? moment(x.end).format('YYYY-M-D-H-m').split("-") : moment(x.start).add(1, 'h').format('YYYY-M-D-H-m').split("-"),
-            "description": parseDesc(x.description)[2],
-            "title": parseDesc(x.description)[1],
-            "categories": parseDesc(x.description)[0],
-            "startOutputType": "local"
-        }
-    });
-    ics.createEvents(data, async(e, v) => {
-        if (!e) {
-            try {
-                console.log('Writing ics...');
-                await fs.writeFileSync(settings.ics_path, v);
-                console.log('Saved to ' + settings.ics_path);
-            } catch (e) {
+    settings.people.forEach(async c => {
+        console.log("Extracting calendar data for " + c.slug);
+        res = await do_request(settings.response_url, "POST", { cookie: `${af[0]};${get_lc(res)}` }, `start=${moment().subtract(1, "month").format("YYYY-MM-DD")}&end=${moment().add(1, "year").format("YYYY")}-12-31&resType=104&calView=month&federationIds%5B%5D=${c.id}`);
+        console.log("Parsing data for " + c.slug);
+        let data = (await res.json()).map(x => {
+            console.log(parseDesc(x.description)[2]);
+            return {
+                "start": moment(x.start).format('YYYY-M-D-H-m').split("-"),
+                "end": moment(x.end).format('YYYY-M-D-H-m').split("-").length > 2 ? moment(x.end).format('YYYY-M-D-H-m').split("-") : moment(x.start).add(1, 'h').format('YYYY-M-D-H-m').split("-"),
+                "description": parseDesc(x.description)[2],
+                "title": parseDesc(x.description)[1] + ' - ' + ((parseDesc(x.description)[2].slice(0,8).match("^PAU*") ) ? (parseDesc(x.description)[2].slice(4,8)) : ""),
+                "categories": parseDesc(x.description)[0],
+                "startOutputType": "local"
+            }
+        });
+
+        ics.createEvents(data, async(e, v) => {
+            if (!e) {
+                try {
+                    console.log('Writing ics for ' + c.slug);
+                    await fs.writeFileSync("" + settings.ics_path + c.slug + ".ics", v);
+                    console.log('Saved to ' + settings.ics_path);
+                } catch (e) {
+                    console.log(e);
+                }
+            } else {
                 console.log(e);
             }
-        } else {
-            console.log(e);
-        }
-    })
+        })
+    });
+
+    
 })();
-});
+
